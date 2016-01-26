cf86ae484a010c42840e66e88781734d9f95cdca
Benjamin <github@tux-fr.net>
2016-01-26 13:18:22 +0100

    Coding...

--- ServiceCloud Email Signature.user.js
+++ ServiceCloud Email Signature.user.js
@@ -5,12 +5,13 @@
 // @downloadURL https://github.com/tuxfre/esko-SC-scripts/raw/master/ServiceCloud%20Email%20Signature.user.js
 // @include     https://esko.my.salesforce.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 document.addEventListener("DOMNodeInserted", function () {
+  jQuery(".cke_wysiwyg_frame").contents().replace("--------------- Original Message ---------------", "Yeepeee<br />--------------- Original Message ---------------");
 },
                           false);

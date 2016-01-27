bc2c00d2b24ac95c77d59cec3d12c8908e41d03b
Benjamin <github@tux-fr.net>
2016-01-27 11:36:52 +0100

    adapt metadata

--- ServiceCloud Local Links.user.js
+++ ServiceCloud Local Links.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name        ServiceCloud Local Links
+// @author      bevi@esko.com
 // @namespace   com.esko.bevi.scloclinks
 // @description Changes links to jobfolders to local links
 // @downloadURL https://github.com/tuxfre/esko-SC-scripts/raw/master/ServiceCloud%20Local%20Links.user.js
 // @include     https://esko.my.salesforce.com/*
-// @version     4
+// @version     5
+// @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       none
 // ==/UserScript==
 

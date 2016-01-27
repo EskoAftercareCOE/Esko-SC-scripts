48834562ef46a99e68e287ae096061d5234df32c
Benjamin <github@tux-fr.net>
2016-01-27 11:38:30 +0100

    adapt metadata

--- ServiceCloud Refresh List.user.js
+++ ServiceCloud Refresh List.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name        ServiceCloud Refresh List
+// @author      bevi@esko.com
 // @namespace   com.esko.bevi.screfresh
 // @description Refreshes ServiceCloud list views
 // @downloadURL https://github.com/tuxfre/esko-SC-scripts/raw/master/ServiceCloud%20Refresh%20List.user.js
 // @include     https://esko.my.salesforce.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version     3
+// @version     4
+// @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       none
 // ==/UserScript==
 

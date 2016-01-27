174d1eaea584d3747d35a640f1aca9d725427ce2
Benjamin <github@tux-fr.net>
2016-01-27 12:40:14 +0100

    try to detect GM_Download and use it

--- ServiceCloud Multi Downloader.user.js
+++ ServiceCloud Multi Downloader.user.js
@@ -15,51 +15,56 @@
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 document.addEventListener("DOMNodeInserted", function () {
-    var attTitle = jQuery("h3[id$='EmailMessageUi$RelatedEmailMessageAttachmentList_title']");
+	var attTitle = jQuery("h3[id$='EmailMessageUi$RelatedEmailMessageAttachmentList_title']");
 
-    if(!attTitle.has("button").length) {
-        attTitle.append("<button name=\"button\" id=\"scmultid\" onclick=\"DownloadAll\" style=\"margin-left: 1em;\">Download all</button>");
-        document.getElementById('scmultid').addEventListener('click', DownloadAll, false);
-    }
+	if(!attTitle.has("button").length) {
+		attTitle.append("<button name=\"button\" id=\"scmultid\" onclick=\"DownloadAll\" style=\"margin-left: 1em;\">Download all</button>");
+		document.getElementById('scmultid').addEventListener('click', DownloadAll, false);
+	}
 }, false);
 
 
 function DownloadAll() {
-    jQuery("a.actionLink:contains('View')").each(function( index ) {
-        var FileName = jQuery(this).attr("title").split("-");
-        SaveToDisk(jQuery(this).attr("href"), trim(FileName[FileName.length-1]));
-        //GM_download(jQuery(this).attr("href"), trim(FileName[FileName.length-1]));
-    });    
+	jQuery("a.actionLink:contains('View')").each(function( index ) {
+		var FileName = jQuery(this).attr("title").split("-");
+		if (typeof GM_download !== 'undefined') {
+			GM_download(jQuery(this).attr("href"), trim(FileName[FileName.length-1]));
+		} else {
+			SaveToDisk(jQuery(this).attr("href"), trim(FileName[FileName.length-1]));
+
+		}
+	});
 }
 
 /// SaveToDisk taken from http://muaz-khan.blogspot.com/2012/10/save-files-on-disk-using-javascript-or.html
 function SaveToDisk(fileURL, fileName) {
-    // for non-IE
-    if (!window.ActiveXObject) {
-        var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';
-        save.download = fileName || 'unknown';
+	// for non-IE
+	if (!window.ActiveXObject) {
+		var save = document.createElement('a');
+		save.href = fileURL;
+		save.target = '_blank';
+		save.download = fileName || 'unknown';
 
-        try {
-            var evt = new MouseEvent('click', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': false
-            });
-            save.dispatchEvent(evt);
-        } catch (e) {
-            window.open(fileURL, fileName);
-        }
+		try {
+			var evt = new MouseEvent('click', {
+				'view': window,
+				'bubbles': true,
+				'cancelable': false
+			});
+			save.dispatchEvent(evt);
+		} catch (e) {
+			window.open(fileURL, fileName);
+		}
 
-        (window.URL || window.webkitURL).revokeObjectURL(save.href);
-    }
+		(window.URL || window.webkitURL).revokeObjectURL(save.href);
+	}
 
-    // for IE < 11
-    else if (!!window.ActiveXObject && document.execCommand) {
-        var _window = window.open(fileURL, '_blank');
-        _window.document.close();
-        _window.document.execCommand('SaveAs', true, fileName || fileURL);
-        _window.close();
-    }
+	// for IE < 11
+	else if (!!window.ActiveXObject && document.execCommand) {
+		var _window = window.open(fileURL, '_blank');
+		_window.document.close();
+		_window.document.execCommand('SaveAs', true, fileName || fileURL);
+		_window.close();
+	}
 }
+

402b8ed530a614d5df44463fc07174c6c7a18865
Benjamin <github@tux-fr.net>
2016-01-27 11:36:00 +0100

    adapt metadata

--- ServiceCloud Email Signature.user.js
+++ ServiceCloud Email Signature.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name        ServiceCloud Email Signature
 // @namespace   com.esko.bevi.scesig
+// @author      bevi@esko.com
 // @description Adds a Signature at the bottom of a ServiceCloud reply
 // @downloadURL https://github.com/tuxfre/esko-SC-scripts/raw/master/ServiceCloud%20Email%20Signature.user.js
 // @include     https://esko.my.salesforce.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version     2
+// @version     3
+// @date        2016-01-27
+// @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       none
 // ==/UserScript==
 

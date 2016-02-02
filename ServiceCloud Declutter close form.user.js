eee2d96eb5d4493c12635ad1ecd10dee230936b3
Benjamin <github@tux-fr.net>
2016-02-02 08:40:34 +0100

    Add new items to the list of objects to hide

--- ServiceCloud Declutter close form.user.js
+++ ServiceCloud Declutter close form.user.js
@@ -6,7 +6,7 @@
 // @downloadURL https://github.com/tuxfre/esko-SC-scripts/raw/master/ServiceCloud%20Declutter%20close%20form.user.js
 // @include     https://esko.my.salesforce.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version     1
+// @version     2
 // @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       none
 // ==/UserScript==
@@ -14,7 +14,7 @@
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 if (/^Case Edit\: ([0-9]{8}) ~ Salesforce - Unlimited Edition$/i.test(jQuery(document).attr('title'))) {
-	var targets = ["#head_2_ep", "#head_3_ep", "#head_4_ep", "#head_5_ep", "#head_6_ep", "#head_7_ep", "#head_8_ep", "#head_9_ep"];
+	var targets = ["#head_2_ep", "#head_3_ep", "#head_4_ep", "#head_5_ep", "#head_6_ep", "#head_7_ep", "#head_8_ep", "#head_9_ep", "#head_11_ep", "#head_1_ep"];
 	jQuery.each(targets, function( index, value ) {
 		var myContents = jQuery(value).next();
 		var myContentID = value.substr(1)+"_contents";

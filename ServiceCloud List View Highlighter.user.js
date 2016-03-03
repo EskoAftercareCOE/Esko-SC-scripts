4679d60a281bc844d13c6962bcccf7aa2a2bd29f
Benjamin <tuxfre@users.noreply.github.com>
2016-03-03 14:16:10 +0100

    only highlight "new" and "pending" cases
    Thanks AVDW for the suggestion!

--- ServiceCloud List View Highlighter.user.js
+++ ServiceCloud List View Highlighter.user.js
@@ -7,7 +7,7 @@
 // @include     /^http(s)?:\/\/(esko\.my\.salesforce\.com)\/([0-9A-Z]+\?)(.*)$/
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js
-// @version     15
+// @version     16
 // @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       GM_addStyle
 // ==/UserScript==
@@ -66,106 +66,109 @@ document.addEventListener('DOMNodeInserted', function () {
 	// for each "line" of the result page we will do a few things...
 	jQuery('.x-grid3-row-table').each(function(index) {
 
-		// we'll use that later to store the tooltip text
-		var toolTip;
+		//if the case status is "New" or "Pending"
+		if ((jQuery(this).find('.x-grid3-col-CASES_STATUS').text().lastIndexOf('New', 0) === 0) || (jQuery(this).find('.x-grid3-col-CASES_STATUS').text().lastIndexOf('Pending', 0) === 0)) {
 
-		// we'll use that to store infos on the style of time settings (EU or US)
-		var hourStyle;
+			// we'll use that later to store the tooltip text
+			var toolTip;
 
-		// first we reset the styles
-		jQuery(this).css('background-color', '');
+			// we'll use that to store infos on the style of time settings (EU or US)
+			var hourStyle;
 
-		// let's fetch and store this log's creation date (regex powaaaa)
-		var dateOpenData = /^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4}) ([0-9]{1,2}):([0-9]{2})(\s?PM|\s?AM)?$/g.exec(jQuery(this).find('.x-grid3-td-CASES_CREATED_DATE').text());
+			// first we reset the styles
+			jQuery(this).css('background-color', '');
 
-		// now let's make a proper date object from what our regex extracted
-		var dateOpenObj;
+			// let's fetch and store this log's creation date (regex powaaaa)
+			var dateOpenData = /^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4}) ([0-9]{1,2}):([0-9]{2})(\s?PM|\s?AM)?$/g.exec(jQuery(this).find('.x-grid3-td-CASES_CREATED_DATE').text());
 
-		// new check for US style dates
-		if (typeof dateOpenData[6] === 'undefined' || dateOpenData[6] === null) {
-			hourStyle = '24H';
-		} else {
-			hourStyle = dateOpenData[6].trim();
-		}
+			// now let's make a proper date object from what our regex extracted
+			var dateOpenObj;
 
-		if (hourStyle == 'PM') {
-			if (dateOpenData[4] != 12) {
-			// PM => hours + 12 & switch month & day
-			dateOpenObj  = new Date(dateOpenData[3],dateOpenData[1]-1,dateOpenData[2],dateOpenData[4]+12,dateOpenData[5]);
+			// new check for US style dates
+			if (typeof dateOpenData[6] === 'undefined' || dateOpenData[6] === null) {
+				hourStyle = '24H';
 			} else {
-			// 12PM => switch month & day
-			dateOpenObj  = new Date(dateOpenData[3],dateOpenData[1]-1,dateOpenData[2],dateOpenData[4],dateOpenData[5]);
+				hourStyle = dateOpenData[6].trim();
 			}
-		} else if (hourStyle == 'AM') {
-			// AM => switch month & day
-			dateOpenObj  = new Date(dateOpenData[3],dateOpenData[1]-1,dateOpenData[2],dateOpenData[4],dateOpenData[5]);
-		} else {
-			// European-style date => let's use it like this
-			dateOpenObj  = new Date(dateOpenData[3],dateOpenData[2]-1,dateOpenData[1],dateOpenData[4],dateOpenData[5]);
-		}
 
-		// and now let's get how many minutes have elapsed since the log was opened
-		var dateDifference = Math.round((dateNow-dateOpenObj)/(1000*60));
-
-		// if the case isn't on Call
-		if ((jQuery(this).find('.x-grid3-col-'+contStatusColClass).text() != 'CA')) {
-
-			// let's add a tooltip to the table that shows how long the log was open
-			if (Math.round(60-dateDifference) > 0) {
-				toolTip = 'This log has been opened for ' + dateDifference + ' minutes, ' + Math.round(60-dateDifference) + ' minutes left!';
+			if (hourStyle == 'PM') {
+				if (dateOpenData[4] != 12) {
+					// PM => hours + 12 & switch month & day
+					dateOpenObj  = new Date(dateOpenData[3],dateOpenData[1]-1,dateOpenData[2],dateOpenData[4]+12,dateOpenData[5]);
+				} else {
+					// 12PM => switch month & day
+					dateOpenObj  = new Date(dateOpenData[3],dateOpenData[1]-1,dateOpenData[2],dateOpenData[4],dateOpenData[5]);
+				}
+			} else if (hourStyle == 'AM') {
+				// AM => switch month & day
+				dateOpenObj  = new Date(dateOpenData[3],dateOpenData[1]-1,dateOpenData[2],dateOpenData[4],dateOpenData[5]);
 			} else {
-				toolTip = 'This log has been opened for ' + dateDifference + ' minutes, ' + Math.round(dateDifference-60) + ' minutes late!';
+				// European-style date => let's use it like this
+				dateOpenObj  = new Date(dateOpenData[3],dateOpenData[2]-1,dateOpenData[1],dateOpenData[4],dateOpenData[5]);
 			}
 
-			jQuery(this).attr('title', toolTip);
-			if (usejQueryUI) {
-				jQuery(this).tooltip({show:{effect:'fade'}, hide:{effect:'fade'}, track:true});
-			}
+			// and now let's get how many minutes have elapsed since the log was opened
+			var dateDifference = Math.round((dateNow-dateOpenObj)/(1000*60));
 
-			// let's color the lines
-			if (dateDifference > 60 ) {
-				// if the log is older than one hour => black background
-				jQuery(this).css('background-color', 'rgba(0, 0, 0, 0.33)');
+			// if the case isn't on Call
+			if ((jQuery(this).find('.x-grid3-col-'+contStatusColClass).text() != 'CA')) {
 
-			} else if (dateDifference > 50 ) {
-				// if the log is older than 50 min => red background
-				jQuery(this).css('background-color', 'rgba(255, 0, 0, 0.33)');
+				// let's add a tooltip to the table that shows how long the log was open
+				if (Math.round(60-dateDifference) > 0) {
+					toolTip = 'This log has been opened for ' + dateDifference + ' minutes, ' + Math.round(60-dateDifference) + ' minutes left!';
+				} else {
+					toolTip = 'This log has been opened for ' + dateDifference + ' minutes, ' + Math.round(dateDifference-60) + ' minutes late!';
+				}
 
-			} else if (dateDifference > 40 ) {
-				// if the log is older than 40 min => red background
-				jQuery(this).css('background-color', 'rgba(255, 128, 0, 0.33)');
+				jQuery(this).attr('title', toolTip);
+				if (usejQueryUI) {
+					jQuery(this).tooltip({show:{effect:'fade'}, hide:{effect:'fade'}, track:true});
+				}
 
-			} else if (dateDifference > 30 ) {
-				// if the log is older than 30 min => red background
-				jQuery(this).css('background-color', 'rgba(0, 255, 0, 0.33)');
-			}
+				// let's color the lines
+				if (dateDifference > 60 ) {
+					// if the log is older than one hour => black background
+					jQuery(this).css('background-color', 'rgba(0, 0, 0, 0.33)');
 
-		} else {
-			// the case is "On call" let's use a proper highliting scheme
-			// here we count in days, not minutes, let's convert
-			var dateDifferenceDays = Math.round(dateDifference/(60*24));
-			// let's add a tooltip to the table that shows how long the log was open
-			if (Math.round(7-dateDifferenceDays) > 0) {
-				toolTip = 'This log has been opened for ' + dateDifferenceDays + ' days, ' + Math.round(7-dateDifferenceDays) + ' days left!';
+				} else if (dateDifference > 50 ) {
+					// if the log is older than 50 min => red background
+					jQuery(this).css('background-color', 'rgba(255, 0, 0, 0.33)');
+
+				} else if (dateDifference > 40 ) {
+					// if the log is older than 40 min => red background
+					jQuery(this).css('background-color', 'rgba(255, 128, 0, 0.33)');
+
+				} else if (dateDifference > 30 ) {
+					// if the log is older than 30 min => red background
+					jQuery(this).css('background-color', 'rgba(0, 255, 0, 0.33)');
+				}
+
 			} else {
-				toolTip = 'This log has been opened for ' + dateDifferenceDays + ' days, ' + Math.round(dateDifferenceDays-7) + ' days late!';
-			}
-			jQuery(this).attr('title', toolTip);
-			if (usejQueryUI) {
-				jQuery(this).tooltip({show:{effect:'fade'}, hide:{effect:'fade'}, track:true});
-			}
+				// the case is "On call" let's use a proper highliting scheme
+				// here we count in days, not minutes, let's convert
+				var dateDifferenceDays = Math.round(dateDifference/(60*24));
+				// let's add a tooltip to the table that shows how long the log was open
+				if (Math.round(7-dateDifferenceDays) > 0) {
+					toolTip = 'This log has been opened for ' + dateDifferenceDays + ' days, ' + Math.round(7-dateDifferenceDays) + ' days left!';
+				} else {
+					toolTip = 'This log has been opened for ' + dateDifferenceDays + ' days, ' + Math.round(dateDifferenceDays-7) + ' days late!';
+				}
+				jQuery(this).attr('title', toolTip);
+				if (usejQueryUI) {
+					jQuery(this).tooltip({show:{effect:'fade'}, hide:{effect:'fade'}, track:true});
+				}
 
-			// let's color the lines
-			if (dateDifferenceDays > 7 ) {
-				// if the log is older than 7 days => black background
-				jQuery(this).css('background-color', 'rgba(0, 0, 0, 0.33)');
+				// let's color the lines
+				if (dateDifferenceDays > 7 ) {
+					// if the log is older than 7 days => black background
+					jQuery(this).css('background-color', 'rgba(0, 0, 0, 0.33)');
 
-			} else if (dateDifferenceDays > 2 ) {
-				// if the log is older than 2 days => blue background
-				jQuery(this).css('background-color', 'rgba(0, 0, 255, 0.33)');
+				} else if (dateDifferenceDays > 2 ) {
+					// if the log is older than 2 days => blue background
+					jQuery(this).css('background-color', 'rgba(0, 0, 255, 0.33)');
+				}
 			}
 		}
-
 		// let's check if some cases are pending (finance, quote, sinterklaas...)
 		if ((jQuery(this).find('.x-grid3-col-CASES_STATUS').text().lastIndexOf('Pending', 0) === 0)) {
 			// but only if action required isn't checked

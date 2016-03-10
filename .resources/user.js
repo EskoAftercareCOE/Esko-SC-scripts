98f7a27acb5c942f4a1480a379b0537b537bb9d4
Benjamin <tuxfre@users.noreply.github.com>
2016-03-10 15:03:16 +0100

    corrections to protos

--- .resources/user.js
+++ .resources/user.js
@@ -1,7 +1,9 @@
-//activate localfile links for SFDC
+// allow using local file links for SFDC
 user_pref("capability.policy.policynames", "localfilelinks");
 user_pref("capability.policy.localfilelinks.sites", "https://esko.my.salesforce.com/");
 user_pref("capability.policy.localfilelinks.checkloaduri.enabled", "allAccess");
-//enable using windows explorer / mac os finder to handle file:// links
+// enable asking for a specific handler for file:// links (windows explorer / mac os finder)
 user_pref("network.protocol-handler.expose.file", false);
 user_pref("network.protocol-handler.external.file", true);
+user_pref("network.protocol-handler.expose.smb", false);
+user_pref("network.protocol-handler.external.smb", true);

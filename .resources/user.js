66a4c3f0fbc0525fb689079878181d37dbe1012a
Benjamin <tuxfre@users.noreply.github.com>
2016-03-10 15:11:27 +0100

    Update user.js

--- .resources/user.js
+++ .resources/user.js
@@ -4,6 +4,8 @@ user_pref("capability.policy.localfilelinks.sites", "https://esko.my.salesforce.
 user_pref("capability.policy.localfilelinks.checkloaduri.enabled", "allAccess");
 // enable asking for a specific handler for file:// links (windows explorer / mac os finder)
 user_pref("network.protocol-handler.expose.file", false);
+user_pref("network.protocol-handler.app.file", "C:\windows\explorer.exe");
 user_pref("network.protocol-handler.external.file", true);
 user_pref("network.protocol-handler.expose.smb", false);
+user_pref("network.protocol-handler.app.smb", "/System/Library/CoreServices/Finder.app/Contents/MacOS/Finder");
 user_pref("network.protocol-handler.external.smb", true);

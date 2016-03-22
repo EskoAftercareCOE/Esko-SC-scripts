b3d475140439821dbcef28e125281b97fcceaed5
Benjamin <tuxfre@users.noreply.github.com>
2016-03-22 12:46:43 +0100

    adapted link to new server (see issue #9)

--- ServiceCloud Local Links.user.js
+++ ServiceCloud Local Links.user.js
@@ -8,7 +8,7 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version     7
+// @version     8
 // @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -25,7 +25,7 @@ function links() {
 	jQuery("a[title^='Job Folder']").parent().css( "background-color", "rgba(255,255,0,0.5)" );
 	jQuery("a[title^='Job Folder']").prepend('<img src="'+ folderIcon +'" style="margin-right: 0.5em; height: 1em; width: 1em; vertical-align: baseline;">');
 	var caseID = jQuery(document).find("title").text().match(/^Case: ([0-9]{8}).*$/);
-	var jobFolderBasePath = 'esko-graphics.com/globalstorage/CC-SW-Problemlog-Gent/' + caseID[1].slice(0, -3) + '000-' + caseID[1].slice(0, -3) + '999/' + caseID[1] + '/';
+	var jobFolderBasePath = 'CC-SW-Problemlog-Gent/CC-SW-Problemlog-Gent/' + caseID[1].slice(0, -3) + '000-' + caseID[1].slice(0, -3) + '999/' + caseID[1] + '/';
 	jQuery("a[title='Job Folder Windows']").attr('href', 'file://///' + jobFolderBasePath);
 	jQuery("a[title='Job Folder Mac']").attr('href', 'smb://' + jobFolderBasePath);
 }

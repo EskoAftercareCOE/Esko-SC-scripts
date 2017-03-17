13a627afe0161ab8735c699d8f5df58991360a6c
Benjamin <tuxfre@users.noreply.github.com>
2017-03-17 12:20:02 +0100

    only show tooltip for images
    there's code for text as well but SFDC forces the download, will have to work on this

--- ServiceCloud Better Files Widget.user.js
+++ ServiceCloud Better Files Widget.user.js
@@ -9,7 +9,7 @@
 // @require     https://raw.githubusercontent.com/sizzlemctwizzle/GM_config/master/gm_config.js
 // @require     https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
 // @require     http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js
-// @version     2
+// @version     3
 // @downloadURL https://github.com/tuxfre/esko-SC-scripts/raw/master/ServiceCloud%20Better%20Files%20Widget.user.js
 // @icon        data:image/gif;base64,R0lGODlhIAAgAKIHAAGd3K/CNOz4/aje8zGv3HLJ63PAsv///yH5BAEAAAcALAAAAAAgACAAQAPGeLrc/k4MQKu9lIxRyhaCIhBVYAZGdgZYCwwMKLmFLEOPDeL8MgKEFXBFclkIoMJxRTRmaqGedEqtigSFYmYgGRAInV3vlzGsDFonoCZSAlAAQyqeKrDUFK7FHCDJh3B4bBJueBYeNmOEX4hRVo+QkZKTV4SNBzpiUlguXxcamRFphhhgmgIVQSZyJ6NGgz98Jl9npFwTFLOlJqQ1FkIqJ4ZIZIAEfGi6amyYacdnrk8dXI6YXVlGX4yam9hHXJTWOuHk5RAJADs=
 // @grant       GM_getValue
@@ -17,13 +17,14 @@
 // @grant       GM_log
 // ==/UserScript==
 
+var imageformats = ["jpg", "jpeg", "gif", "png", "pdf", "svg", "tiff", "tif", "bmp", "ico"];
+var textformats = ["txt", "xml", "log", "rtf"];
+
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 $("head").append ('<link href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/redmond/jquery-ui.min.css" rel="stylesheet" type="text/css">');
-$("head").append ('<style>.ui-tooltip{ max-width: 50%; width: 50%; }</style>');
-
-
+$("head").append ('<style>.ui-tooltip{ max-width: 50%; width: 50%; max-height: 90%; overflow: hidden;}</style>');
 
 $.expr[':'].textEquals = $.expr.createPseudo(function(arg) {
 	return function( elem ) {
@@ -41,16 +42,30 @@ function init() {
 
 	$('div.eFilesWidgetFileNameContainer > a').each(function(index) {
 		var fileID = $(this).attr("href").match(/javascript:srcUp\('%2F([^%]+)%3F[^']+'\);/)[1];
+		var fileName = $(this).text().trim();
+		var fileExtension = fileName.substr((~-fileName.lastIndexOf(".") >>> 0) + 2).toLowerCase();
 		var directFileURL = $('a[onclick*="'+fileID+'"]:textEquals("Download")').attr("onclick").match(/entityFilesComponent\.performAction\(\'[^']+\', \'[^']+\', \'[^']+\', \'([^']+)\'/)[1];
 		$(this).attr("href", directFileURL);
-		$(this).tooltip({
-			position: {
-				my: "center top",
-				at: "center top+5%",
-				of: "div.centerContent"
-			},
-			content: '<div><img src="'+directFileURL+'" style="width: 100%; height: 100%;" /></div>'
-		});
+		if ($.inArray(fileExtension, imageformats) !== -1) {
+			$(this).tooltip({
+				position: {
+					my: "center top",
+					at: "center top+5%",
+					of: "div.centerContent"
+				},
+				content: '<div><img src="'+directFileURL+'" style="width: 100%; height: 100%;" /></div>'
+			});
+		} /*else if ($.inArray(fileExtension, textformats) !== -1) {
+			$(this).tooltip({
+				position: {
+					my: "center top",
+					at: "center top+5%",
+					of: "div.centerContent"
+				},
+				content: '<div><iframe src="'+directFileURL+'" style="width: 100%; height: 100%;" /></div>'
+			});
+
+		}*/ /* SFDC forces the download regardless */
 	});
 }
 
